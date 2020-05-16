ee9cd5eb802a3315a82ad55b4b1cb472495864bc
xiaoxiao <460046653@qq.com>
2020-05-16 10:08:16 +0800

    create <Layoutãã> componet

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="wrapper">
-    <div class="content">
-      Labels.vue
-    </div>
-    <Nav />
-  </div>
+  <Layout>
+    <p>Labels.vue</p>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -14,13 +11,5 @@
 </script>
 
 <style lang="scss" scoped>
-  .wrapper{
-    display: flex;  //使用flex布局
-    flex-direction: column; //方向是上下形式
-    height: 100vh;  //高度100 vh是view Height
-  }
-  .content{
-    overflow: auto;   //超出set height 显示滚动条
-    flex-grow: 1;   //该属性指定flex容器剩余空间多少应该分配给项目
-  }
+
 </style>
