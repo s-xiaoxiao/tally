f91430cbf5c379dd8b3df668c8d7cc8e8d21e077
xiaoxiao <460046653@qq.com>
2020-05-16 15:15:52 +0800

    update viewport and remove the fill of label.svg and nav add active-class

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,19 @@
 <template>
   <nav>
-    <router-link to="money" class="item">
-      <Icon name="money" class="icon"/>
+    <router-link to="money" class="item" active-class="selected">
+      <Icon name="money"/>
 
       记账
     </router-link>
 
-    <router-link to="labels" class="item">
-      <Icon name="label" class="icon"/>
+    <router-link to="labels" class="item" active-class="selected">
+      <Icon name="label"/>
 
       标签
     </router-link>
 
-    <router-link to="statistics" class="item">
-      <Icon name="statistics" class="icon"/>
+    <router-link to="statistics" class="item" active-class="selected">
+      <Icon name="statistics"/>
       统计
     </router-link>
   </nav>
@@ -32,16 +32,20 @@
     flex-direction: row;
     font-size: 12px;
     > .item{
+      color: #AEB6BF ;
       padding: 2px 0;
       width: 33.33333%;
       display: flex;
       justify-content: center;
-      /*align-items: center;*/
+      align-items: center;
       flex-direction: column;
       .icon{
         width: 32px;
         height: 32px;
       }
+    }
+    > .item.selected{
+      color:inherit;
     }
   }
 </style>
