3a6168aa59e8ea139b31b513a32eba175890e563
xiaoxiao <460046653@qq.com>
2020-05-18 14:15:30 +0800

    解决svg自带fill的问题

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,8 @@ module.exports = {
       .test(/\.svg$/)
       .include.add(dir).end() //包含 icons 目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+      .use('svgo-loader').loader('svgo-loader') //使用svgo 对svg文件进行优化
+      .tap(options =>({...options,plugins:[{removeAttrs:{attrs:'fill'}}]})).end()   //删除自带fill属性
 
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite:true}])
