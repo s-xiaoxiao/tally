22235f6558077cfc7952f50f56ecd66d78b79290
xiaoxiao <460046653@qq.com>
2020-05-23 18:18:24 +0800

    按照 vue-class-component 文档使用props失败

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -2,55 +2,73 @@
   <div>
     <ul class="types">
       <li :class="type === '-' && 'selected'"
-          @click="seleteType('-')">支出</li>
+          @click="selectType('-')">支出
+      </li>
       <li :class="type === '+' && 'selected'"
-          @click="seleteType('+')">收入</li>
+          @click="selectType('+')">收入
+      </li>
     </ul>
   </div>
 
 </template>
 
-<script>
-  export default {
-    name: 'Types',
-    props:['xxx'],
-    data(){
-      return{
-        type:'-'
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Types extends Vue {
+    type = '-'; // ‘-’ 支出 ‘+’ 收入
+    selectType(type: string) {
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknow');
       }
-    },
-    mounted(){
-      console.log(this.xxx)
-    },
-    methods:{
-      seleteType(type){
-        if(type !== '-' && type !== '+'){
-          throw new Error('type is unknow')
-        }
-        this.type = type;
-      }
+      this.type = type;
     }
-  };
+  }
+
+  // export default {
+  //   name: 'Types',
+  //   props:['xxx'],
+  //   data(){
+  //     return{
+  //       type:'-'
+  //     }
+  //   },
+  //   mounted(){
+  //     console.log(this.xxx)
+  //   },
+  //   methods:{
+  //     seleteType(type){
+  //       if(type !== '-' && type !== '+'){
+  //         throw new Error('type is unknow')
+  //       }
+  //       this.type = type;
+  //     }
+  //   }
+  // };
 </script>
 
 <style lang="scss" scoped>
-  .types{
+  .types {
     background: #c4c4c4;
     display: flex;
     text-align: center;
     font-size: 24px;
-    > li{
+
+    > li {
       width: 50%;
       height: 64px;
       line-height: 64px;
       display: flex;
-      align-items:center;
+      align-items: center;
       justify-content: center;
       position: relative;
-      &.selected::after{
+
+      &.selected::after {
         position: absolute;
         content: '';
-        bottom:0;
+        bottom: 0;
         left: 0;
         width: 100%;
         height: 4px;
