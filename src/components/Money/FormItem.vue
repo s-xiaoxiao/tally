6d288cadc8db12d5ba2d906279f5c78a2149e669
xiaoxiao <460046653@qq.com>
2020-06-09 17:41:25 +0800

    修复 value 被赋值的问题

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -2,7 +2,7 @@
   <div >
     <label class="notes">
       <span class="name">{{this.fieldName}}</span>
-      <input type="text" v-model="value" :placeholder="this.placeholder"/>
+      <input type="text" @input="onValueChanged($event.target.value)" :placeholder="this.placeholder"/>
     </label>
   </div>
 </template>
